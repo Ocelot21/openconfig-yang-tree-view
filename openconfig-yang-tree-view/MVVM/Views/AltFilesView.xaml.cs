5ab7fdc04ca69b61e2983ec3cec83ef29207a02a
Afan Bajraktarević <afan.bajraktarevic@skyline.be>
2023-09-19 17:35:17 +0200

    Core added for MVVM pattern and changes made to UI

--- openconfig-yang-tree-view/MVVM/Views/AltFilesView.xaml.cs
+++ openconfig-yang-tree-view/MVVM/Views/AltFilesView.xaml.cs
@@ -1,5 +1,4 @@
-﻿using openconfig_yang_tree_view.MVVM.Views;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,23 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace openconfig_yang_tree_view
+namespace openconfig_yang_tree_view.MVVM.Views
 {
     /// <summary>
-    /// Interaction logic for AltWindow.xaml
+    /// Interaction logic for AltFilesView.xaml
     /// </summary>
-    public partial class AltWindow : Window
+    public partial class AltFilesView : UserControl
     {
-        public AltWindow()
+        public AltFilesView()
         {
             InitializeComponent();
-        }
-
-        private void RadioButton_Checked(object sender, RoutedEventArgs e)
-        {
-            contentControl.Content = new AltFilesView();
         }
     }
 }
